abbbea0f6f54589b07247af5a316bc5b0577ef3d
Adarsh Kanojiya <9045762344adarsh@gmail.com>
2020-06-21 12:20:46 +0530

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,15 +3,19 @@ const jwt = require("jsonwebtoken");
 
 
 const auth = async (req,res,next)=>{
-    const token = req.signedCookies["auth-token"] || req.header("Authorization").replace("Bearer ","");
-    const decoded = await jwt.verify(token,process.env.JWT_SECRETE);
-    const user = await User.findOne({_id:decoded._id,"tokens.token":token}); //.(dot) operator to find objs in array
-    if(!user){
-        next(new Error("Please login!"));
+    try{
+        const token = req.signedCookies["auth-token"] || req.header("Authorization").replace("Bearer ","");
+        const decoded = await jwt.verify(token,process.env.JWT_SECRETE);
+        const user = await User.findOne({_id:decoded._id,"tokens.token":token}); //.(dot) operator to find objs in array
+        if(!user){
+            next(new Error("Please login!"));
+        }
+        req.user = user;
+        req.token =token;
+        next();
+    }catch(e){
+        next(new Error(e));
     }
-    req.user = user;
-    req.token =token;
-    next();
 };
 
 
